0700d76cd6f772f62c8a4fb035d2bf223fafadf3
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-06-24 17:21:45 +0200

    tmp-dev: temporarily add verbose logging for dev/debug purposes

--- packages/gatsby/src/utils/service-locks.ts
+++ packages/gatsby/src/utils/service-locks.ts
@@ -1,3 +1,5 @@
+const chalk = require(`chalk`)
+
 const locksLookup = new Map<string, ServiceLock>()
 const pendingRuns = new Map<
   string,
@@ -6,6 +8,54 @@ const pendingRuns = new Map<
     callback: Function
   }
 >()
+
+let lastStatus = ``
+
+function log(...args: any[]): void {
+  const extraLogs: any[] = []
+
+  // status of services
+  const newStatusBuilder: {
+    [record: string]: "IDLE" | "PENDING" | "RUNNING"
+  } = {}
+  locksLookup.forEach(runningLock => {
+    newStatusBuilder[runningLock.name] =
+      runningLock.isRunning > 0
+        ? `RUNNING`
+        : runningLock.isPending
+        ? `PENDING`
+        : `IDLE`
+  })
+
+  const newStatus = JSON.stringify(newStatusBuilder)
+    .replace(/RUNNING/g, chalk.green(`RUNNING`))
+    .replace(/PENDING/g, chalk.yellow(`PENDING`))
+    .replace(/IDLE/g, chalk.gray(`IDLE`))
+
+  if (newStatus !== lastStatus) {
+    extraLogs.push(chalk.cyan(`STATUS CHANGED`), newStatus)
+    lastStatus = newStatus
+  } else {
+    extraLogs.push(chalk.gray(`NOTHING CHANGED`), newStatus)
+  }
+
+  // pending execution
+  const pendingExecutions: string[] = []
+  pendingRuns.forEach((_pendingRun, serviceName) => {
+    pendingExecutions.push(serviceName)
+  })
+
+  if (pendingExecutions.length > 0) {
+    extraLogs.push(
+      chalk.blueBright(`PENDING RUNS`),
+      pendingExecutions.join(`, `)
+    )
+  } else {
+    extraLogs.push(chalk.gray(`NO PENDING RUNS`))
+  }
+
+  console.log(chalk.magenta(`[lock]`), ...args, ...extraLogs)
+}
 
 function getRunningDeps(
   dependencies: string[],
@@ -46,15 +96,23 @@ class ServiceLock {
 
   markAsPending(): void {
     this.isPending = true
+
+    log(
+      `[${this.name}] ${chalk.yellow(`mark as pending`)}` // (reason: "${reason}")`
+    )
   }
 
   markStartRun(): void {
     this.isPending = false
     this.isRunning++
+
+    log(`[${this.name}] ${chalk.green(`mark start run`)}`)
   }
 
   markEndRun(): void {
     this.isRunning--
+
+    log(`[${this.name}] ${chalk.red(`mark end run`)}`)
 
     if (this.isRunning === 0 && !this.isPending) {
       // if this service is no longer running - see if any pending dependant runs
@@ -62,6 +120,12 @@ class ServiceLock {
       pendingRuns.forEach((pendingRun, serviceName) => {
         const runningDeps = getRunningDeps(pendingRun.deps)
         if (runningDeps.size === 0) {
+          log(
+            `Delayed run "${serviceName}" (not blocked anymore by any of [${pendingRun.deps.join(
+              `, `
+            )}])`
+          )
+
           pendingRuns.delete(serviceName)
           pendingRun.callback()
         }
@@ -82,11 +146,18 @@ class ServiceLock {
     }
 
     if (pendingRuns.has(this.name)) {
+      log(
+        `Discarding queued run for "${this.name}" because run is already pending`
+      )
       return
     }
 
     const runningDeps = getRunningDeps(this.deps)
     if (runningDeps.size === 0) {
+      log(
+        `Run "${this.name}" (not blocked by any of [${this.deps.join(`, `)}])`
+      )
+
       run()
       return
     }
@@ -95,6 +166,12 @@ class ServiceLock {
       deps: this.deps,
       callback: enqueue,
     })
+
+    log(
+      `Delay run for "${this.name}" because [${Array.from(runningDeps).join(
+        `, `
+      )}]`
+    )
   }
 }
 
